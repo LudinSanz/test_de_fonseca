056795a5683c168c59fa3f111b7fe9de83ef4c3f
Ludin Solis <ludinsolis@example.com>
2026-08-05 10:20:27 -0600

    Fix HomeScreen import in register_screen.dart

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import '../constants/colors.dart';
 import 'login_screen.dart';
+import 'home_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
